2e905f2eb151b83d55b65fb95f51f021801876bb
EvgenijBaltais <evgenij.baltais@gmail.com>
2024-02-19 20:45:57 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rehau</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="wrapper">
+
+        <header class = "header section">
+            <div class="logo">
+                <img src="images/logo.png" alt="" class="logo-pic">
+            </div>
+            <nav class = "nav">
+                <ul class = "nav-list">
+                    <li class = "nav-list-item">
+                        <a href="" class="nav-list-link">Общая информация</a>
+                    </li>
+                    <li class = "nav-list-item">
+                        <a href="" class="nav-list-link">Программа «СПК»</a>
+                    </li>
+                    <li class = "nav-list-item">
+                        <a href="" class="nav-list-link">Программа «ИНЖЕНЕРНЫЕ СИСТЕМЫ»</a>
+                    </li>
+                    <li class = "nav-list-item">
+                        <a href="" class="nav-list-link">«МЕБЕЛЬНЫЕ СИСТЕМЫ»</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <section class = "section top-section">
+            <h1 class = "h1">Приветствуем вас на ежегодной Конференции сотрудников отделов сбыта</h1>
+            <div class = "top-month">
+                <span class = "top-month-text">Санкт-Петербург, февраль, 2024</span>
+            </div>
+        </section>
+
+        <section class = "section text-content">
+            <div class="text-content-left">
+                <div class = "text-content-item">
+                    <div class = "content-programm-text">
+                        <p class = "content-programm-title">Программа</p>
+                    </div>
+                    <div class = "content-programm-info">
+                        <p class = "content-programm-title">26.02.2024</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-red">Выездной гала-ужин.</p>
+                        <p class = "content-programm-red">Выездной гала-ужин.</p>
+                    </div>
+                </div>
+                <div class = "text-content-item">
+                    <div class = "content-programm-text">
+                        <p class = "content-programm-title">Программа</p>
+                    </div>
+                    <div class = "content-programm-info">
+                        <p class = "content-programm-title">26.02.2024</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-red">Выездной гала-ужин.</p>
+                        <p class = "content-programm-red">Выездной гала-ужин.</p>
+                    </div>
+                </div>
+                <div class = "text-content-item">
+                    <div class = "content-programm-text">
+                        <p class = "content-programm-title">Программа</p>
+                    </div>
+                    <div class = "content-programm-info">
+                        <p class = "content-programm-title">26.02.2024</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-p">9.00 - 13.00 - общая часть</p>
+                        <p class = "content-programm-red">Выездной гала-ужин.</p>
+                        <p class = "content-programm-red">Выездной гала-ужин.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-content-right">
+                <p class = "content-programm-title">Организаторы мероприятия</p>
+                <div class = "content-organisators-item">
+                    <p class = "content-programm-p">Татьяна Малкина</p>
+                    <p class = "content-programm-p">+7 926 800-78-60</p>
+                    <a class = "content-programm-link" href="mailto:tatjana.malkina@rhsolutions.ru">tatjana.malkina@rhsolutions.ru</a>
+                </div>
+                <div class = "content-organisators-item">
+                    <p class = "content-programm-p">Татьяна Малкина</p>
+                    <p class = "content-programm-p">+7 926 800-78-60</p>
+                    <a class = "content-programm-link" href="mailto:tatjana.malkina@rhsolutions.ru">tatjana.malkina@rhsolutions.ru</a>
+                </div>
+                <div class = "content-organisators-item">
+                    <p class = "content-programm-p">Татьяна Малкина</p>
+                    <p class = "content-programm-p">+7 926 800-78-60</p>
+                    <a class = "content-programm-link" href="mailto:tatjana.malkina@rhsolutions.ru">tatjana.malkina@rhsolutions.ru</a>
+                </div>
+            </div>
+        </section>
+
+    </div>
+    
+</body>
+</html>
